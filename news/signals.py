d0ee199e071e1dc19ebed31ac3393b6b7b62d6ec
Karolina <123109500+KarolinaLikhacheva@users.noreply.github.com>
2023-10-29 23:20:56 +0300

    Add files via upload

---
+++ news/signals.py
@@ -0,0 +1,31 @@
+from django.db.models.signals import post_save, m2m_changed
+
+from django.dispatch import receiver
+from .models import PostCategory
+from .tasks.basic import new_post_subscription
+from django.contrib.auth.models import User, Group
+from django.core.mail import send_mail
+
+
+#@receiver(post_save, sender=User)
+#def add_to_common_group(sender, instance, created, **kwargs):
+#    if created:
+#        common_group, created = Group.objects.get_or_create(name='common')
+#        common_group.user_set.add(instance)
+
+
+@receiver(m2m_changed, sender=PostCategory)     # уведомляем пользователя о новом посте
+def notify_subscribers(sender, instance, **kwargs):
+    if kwargs['action'] == 'post_add':
+        pass
+        new_post_subscription(instance)
+
+#@receiver(post_save, sender=User)
+#def send_welcome_email(sender, instance, created, **kwargs):
+#    if created:
+#        subject = 'Добро пожаловать в наше приложение'
+#        message = 'Здравствуйте, спасибо за регистрацию в нашем приложении. Надеемся, вам здесь понравится!'
+#        from_email = l-karolina-a@mail.ru'  # Замените на вашу электронную почту
+#        recipient_list = [instance.email]
+
+#        send_mail(subject, message, from_email, recipient_list, fail_silently=False)
